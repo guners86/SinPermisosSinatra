390c238259f9b1abee69d8ea0d959947026b8f53
guners86 <guners86@hotmail.com>
2015-08-26 19:42:07 -0500

    Version Inicial

---
+++ solution.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+get '/' do
+	if request.env['HTTP_PERMISO'].nil?
+		"Sin Permiso"
+	else
+		"Si lo logramos!"
+	end
+end																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																				
